8ec048b660933d5c7d324a1348a8167613a957fc
Andrew Kvalheim <Andrew@Kvalhe.im>
2022-09-27 16:06:42 -0700

    Refine types

--- src/lib/Plan.ts
+++ src/lib/Plan.ts
@@ -1,4 +1,4 @@
-// Pending samchon/typescript-json#153
+// Pending samchon/typescript-json#153, gcanti/io-ts#322
 
 import { isLeft } from "fp-ts/lib/Either.js";
 import t from "io-ts";
@@ -8,16 +8,14 @@ import type { PowerLevelsEventContent as PowerLevels } from "matrix-bot-sdk";
 
 const RoomPlan: t.Type<RoomPlan> = t.recursion("RoomPlan", () =>
   t.intersection([
-    t.type({ name: t.string, topic: t.string }),
-    t.partial(
-      t.type({
-        avatar: t.string,
-        children: RoomsPlan,
-        destroy: t.boolean,
-        private: t.boolean,
-        suggested: t.boolean,
-      }).props
-    ),
+    t.strict({ name: t.string, topic: t.string }),
+    t.partial({
+      avatar: t.string,
+      children: RoomsPlan,
+      destroy: t.boolean,
+      private: t.boolean,
+      suggested: t.boolean,
+    }),
   ])
 );
 export interface RoomPlan {
@@ -33,29 +31,27 @@ export interface RoomPlan {
 const RoomsPlan = t.record(t.string, RoomPlan);
 export type RoomsPlan = Record<string, RoomPlan>;
 
-const PowerLevels: t.Type<PowerLevels> = t.partial(
-  t.type({
-    ban: t.number,
-    events: t.record(t.string, t.number),
-    events_default: t.number,
-    historical: t.number,
-    invite: t.number,
-    kick: t.number,
-    redact: t.number,
-    state_default: t.number,
-    users: t.record(t.string, t.number),
-    users_default: t.number,
-    notifications: t.partial(t.type({ room: t.number }).props),
-  }).props
-);
+const PowerLevels: t.Type<PowerLevels> = t.partial({
+  ban: t.number,
+  events: t.record(t.string, t.number),
+  events_default: t.number,
+  historical: t.number,
+  invite: t.number,
+  kick: t.number,
+  redact: t.number,
+  state_default: t.number,
+  users: t.record(t.string, t.number),
+  users_default: t.number,
+  notifications: t.partial({ room: t.number }),
+});
 
 const StewardPlan = t.intersection([
-  t.type({ id: t.string, name: t.string }),
-  t.partial(t.type({ avatar: t.string }).props),
+  t.strict({ id: t.string, name: t.string }),
+  t.partial({ avatar: t.string }),
 ]);
 export type StewardPlan = t.TypeOf<typeof StewardPlan>;
 
-const Plan = t.type({
+const Plan = t.strict({
   avatars: t.record(t.string, t.string),
   defaultRoomVersion: t.string,
   homeserver: t.string,
