2d75b24a011b297784778de4d6d30b8883d12e4e
Andrew Kvalheim <Andrew@Kvalhe.im>
2022-11-02 19:59:08 -0700

    Set default time zone

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { readFileSync } from "fs";
+import { Settings } from "luxon";
 import { LogLevel, LogService } from "matrix-bot-sdk";
 import Logger from "./lib/Logger.js";
 import Patch from "./lib/Patch.js";
 import { parsePlan } from "./lib/Plan.js";
 import { env } from "./lib/utilities.js";
+
+const plan = parsePlan(readFileSync("./data/plan.yml", { encoding: "utf8" }));
+
+Settings.defaultZone = plan.timeZone;
 
 LogService.setLogger(new Logger());
 LogService.setLevel(LogLevel.fromString(process.env["LOG_LEVEL"]!));
@@ -13,7 +18,7 @@ LogService.muteModule("Metrics");
 const config = {
   accessToken: env("MATRIX_ACCESS_TOKEN"),
   baseUrl: env("MATRIX_BASE_URL"),
-  plan: parsePlan(readFileSync("./data/plan.yml", { encoding: "utf8" })),
+  plan,
 };
 
 await new Patch(config).start();
