1df87682944c9ec8f9c1ba07fbd9dc665b3e07c2
AJ Jordan <alex@strugee.net>
2026-08-10 21:15:44 -0400

    Add script to list space children

---
+++ contrib/README.md
@@ -0,0 +1,5 @@
+# Patch `contrib/`
+
+This directory contains some random scripts to do stuff that REALLY should be integrated into Patch's codebase, but we had Shit To Do™ to ship the conference and so we hacked these up and moved on with our lives.
+
+Be careful! Here be dragons.
---
+++ contrib/list-space-children.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
+set -Eeuxo pipefail
+
+# Reference: https://spec.matrix.org/latest/client-server-api/
+api() {
+  # TODO: Show full request and response on stderr
+  http --check-status --ignore-stdin \
+    "$1" "https://matrix.seattlematrix.org/_matrix/client/$2" \
+    "Authorization:Bearer $MATRIX_ACCESS_TOKEN" \
+    "${@:3}"
+}
+
+root_space_id="$1"
+
+# Validate credentials
+# # TODO reenable
+#[[ "$(api get 'v3/account/whoami' | jq --raw-output '.user_id')" == '@seagl-bot:seattlematrix.org' ]]
+
+# Spits out sequences of JSON objects. jq handles this fine, as a stream of documents.
+recurse_paginate() {
+  if [[ -z "${1+x}" ]]; then
+    querystring=''
+  else
+    querystring="?from=$1"
+  fi
+
+  res="$(api get "v1/rooms/$root_space_id/hierarchy$querystring")"
+  echo "$res" | jq '.rooms[] | select(.room_type == null)'
+
+  next=$(echo "$res" | jq -r .next_batch)
+  if [[ $next != null ]]; then
+    recurse_paginate $next
+  fi
+}
+recurse_paginate
